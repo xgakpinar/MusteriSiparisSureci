e1cca2dc9b501b7877c5da86a5a19d1796ce81c9
Barış Arslan <barisarslan8@gmail.com>
2017-09-13 23:19:23 +0300

    Feedback 4

--- src/main/java/musteriSiparisSureci/Main.java
+++ src/main/java/musteriSiparisSureci/Main.java
@@ -6,6 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// Tebrikler!
+		// Yaptığın mimari teknik olarak doğru yani her şey istenilen doğrultuda çalışıyor.
+		// Öğrendiklerini de doğru uygulamışsın burada da bir problem yok. 
+		// Belirlediğin mimari doğrultusunda doğru ilerlemişsin yani.
+		
+		// Ancak diğer class lar üzerinde yaptığım yorumları dikkate al.
+		// Sadece şu an süreci doğru anlayıp, tam ihtiyaca yönelik çözümler üreten bir mimari kurman lazım.
+		// Aslında tam olarak danışmanlık ve iyi developer bu şekilde ayrılıyor. Bu da zamanla oturacaktır.
+		// Şu an teknik konulara hakim olman için çalışıyoruz.
+		// Paralelde uygulamaları yazarken süreçsel ihtiyaçları anlayıp onlara da uygun senaryoları düşünmeye çalış.
+		// O zaman birisine ihtiyaç duymadan hataları ve gelebilecek talepleri önceden görmüş olursun. :)
+		
+		// Bu projedeki tek problem, projede gereksiz yük oluşturmuşsun kendine.
+		// Sonuç odaklı olarak gidecek olursak proje tamamlanmış ve başarılı olmuştur.
+		// Ancak proje verimini değerlendirecek olursak, kendine ekstra yük çıkartacak bir mimariye girdiğin için
+		// bu noktayı geliştirmemiz lazım.
+		
 		SalesOrder salesOrder = new SalesOrder("VBELNTEST1");
 		
 		try {
